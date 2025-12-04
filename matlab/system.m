99dc100ff50df88200b517b2d9d6eaec01274b5c
rubinau <rubi.naufal@gmail.com>
2025-12-04 08:42:54 +0700

    added matlab code

---
+++ matlab/system.m
@@ -0,0 +1,46 @@
+M = 0.139;
+m = 0.0315;
+b = 0.1;
+L = 0.3;
+I = 1/3 * m * L^2;
+%I = 0.000945;
+g = 9.8;
+l = L/2;
+q = (M+m)*(I+m*l^2)-(m*l)^2;
+s = tf('s');
+
+P_cart = (((I+m*l^2)/q)*s^2 - (m*g*l/q))/(s^4 + (b*(I + m*l^2))*s^3/q - ((M + m)*m*g*l)*s^2/q - b*m*g*l*s/q);
+
+P_pend = (m*l*s/q)/(s^3 + (b*(I + m*l^2))*s^2/q - ((M + m)*m*g*l)*s/q - b*m*g*l/q);
+
+sys_tf = [P_cart ; P_pend];
+
+p = I*(M+m)+M*m*l^2; %denominator for the A and B matrices
+
+A = [0      1              0           0;
+     0 -(I+m*l^2)*b/p  (m^2*g*l^2)/p   0;
+     0      0              0           1;
+     0 -(m*l*b)/p       m*g*l*(M+m)/p  0];
+B = [     0;
+     (I+m*l^2)/p;
+          0;
+        m*l/p];
+C = [1 0 0 0;
+     0 0 1 0];
+D = [0;
+     0];
+
+states = {'x' 'x_dot' 'phi' 'phi_dot'};
+
+
+inputs = {'u'};
+outputs = {'x'; 'phi'};
+
+sys_ss = ss(A,B,C,D,'statename',states,'inputname',inputs,'outputname',outputs)
+
+%set(sys_tf,'InputName',inputs)
+%set(sys_tf,'OutputName',outputs)
+sys_tf = tf(sys_ss)
+
+sys_tf
+
